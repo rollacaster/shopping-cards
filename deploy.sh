d6f462706fbf27c517cc0ba8c0a93cfde1783637
Thomas Sojka <thsojka@web.de>
2020-06-14 16:51:05 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+mkdir classes;
+clj -e "(compile 'tech.thomas-sojka.ingredients.main)";
+clj -A:fe-build;
+clj -A:uberdeps;
+scp target/ingredients.jar pi@192.168.178.50:/home/pi;
+ssh pi@192.168.178.50 sudo systemctl restart shoppinglist.service
