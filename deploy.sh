7bff83e59797dc8dd53e51268a75e4071882f94e
Thomas Sojka <thsojka@web.de>
2021-02-01 19:48:21 +0100

    Remove deprecated -A

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 mkdir -p classes;
 clj -e "(compile 'tech.thomas-sojka.shopping-cards.main)";
-clj -A:fe-build;
-clj -A:uberdeps;
+clj -M:fe-build;
+clj -M:uberdeps;
 scp target/shopping-cards.jar pi@192.168.178.50:/home/pi;
 ssh pi@192.168.178.50 sudo systemctl restart shoppinglist.service
