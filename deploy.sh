1d7c50624173b6e276fe558e74b84ffeffa42d6e
Thomas Sojka <thomas.sojka@comsysto.com>
2021-10-27 12:42:09 +0200

    Copy db on deploy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ npm run css;
 shadow-cljs compile app;
 clj -M:uberdeps;
 scp target/shopping-cards.jar pi@192.168.178.50:/home/pi;
-ssh pi@192.168.178.50 sudo systemctl restart shoppinglist.service
+scp /Users/thomas/datomic/dev/shopping-cards/* pi@192.168.178.50:/home/pi/datomic/dev/shopping-cards;
+ssh pi@192.168.178.50 sudo systemctl restart shoppinglist.service;
