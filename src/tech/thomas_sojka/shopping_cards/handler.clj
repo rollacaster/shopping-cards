05e3ac11cacf864b16b06bc79e56eec28b005935
Thomas Sojka <thomas.sojka@comsysto.com>
2021-10-28 08:54:29 +0200

    Format

--- src/tech/thomas_sojka/shopping_cards/handler.clj
+++ src/tech/thomas_sojka/shopping_cards/handler.clj
@@ -16,13 +16,13 @@
    (GET "/" [] (resource-response "index.html" {:root "public"}))
    (GET "/recipes" [] {:status 200 :body (vec (filter (comp not :inactive) (load-recipes))) :headers {"Content-type" "application/edn"}})
    (GET "/recipes/:recipe-id/ingredients" [recipe-id]
-     (pr-str (ingredients-for-recipe recipe-id)))
+        (pr-str (ingredients-for-recipe recipe-id)))
    (GET "/ingredients" [recipe-ids]
-     (pr-str (ingredients-for-recipes ((if (vector? recipe-ids) set hash-set) recipe-ids))))
+        (pr-str (ingredients-for-recipes ((if (vector? recipe-ids) set hash-set) recipe-ids))))
    (POST "/shopping-card" request
-     {:status 201
-      :body (create-klaka-shopping-card (:body-params request))
-      :headers {"Content-type" "application/edn"}})
+         {:status 201
+          :body (create-klaka-shopping-card (:body-params request))
+          :headers {"Content-type" "application/edn"}})
    (route/not-found "<h1>Page not found</h1>")))
 
 (def app
