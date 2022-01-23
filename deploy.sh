e2c3a8ae30c7fb9b53d022b4a5e745950becdb80
Thomas Sojka <thsojka@web.de>
2022-01-23 16:33:34 +0100

    Do not override db on deploy

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,4 @@ npm run css;
 shadow-cljs compile app;
 clj -M:uberdeps;
 scp target/shopping-cards.jar pi@ubi-hub:/home/pi;
-scp /Users/thomas/datomic/dev/shopping-cards/* pi@ubi-hub:/home/pi/datomic/dev/shopping-cards;
 ssh pi@ubi-hub sudo systemctl restart shoppinglist.service;
