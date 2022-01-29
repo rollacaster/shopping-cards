c670a816587e51fd36d80a28883bbdfda793ebdc
Thomas Sojka <thsojka@web.de>
2022-01-29 15:32:51 +0100

    Remove old file based db

--- src/server/tech/thomas_sojka/shopping_cards/migrate.clj
+++ src/server/tech/thomas_sojka/shopping_cards/migrate.clj
@@ -1,27 +1,7 @@
 (ns tech.thomas-sojka.shopping-cards.migrate
-  (:require [clojure.java.io :as io]
-            [clojure.string :as str]
-            [datomic.client.api :as d]
+  (:require [datomic.client.api :as d]
             [hodur-datomic-schema.core :as hodur-datomic]
             [tech.thomas-sojka.shopping-cards.schema :refer [meta-db]]))
-
-(defn load-edn [path] (read-string (slurp (io/resource path))))
-(defn load-recipes [] (load-edn "recipes.edn"))
-(defn load-ingredients [] (load-edn "ingredients.edn"))
-(defn load-cooked-with [] (load-edn "cooked-with.edn"))
-
-(defn keywordify [name]
-  (str/lower-case (str/replace name " " "-")))
-
-(defn recipe-name [id]
-  (first (->> (load-recipes)
-              (filter #(= (:id %) id))
-              (map :name))))
-
-(defn ingredient-name [id]
-  (first (->> (load-ingredients)
-              (filter #(= (:id %) id))
-              (map :name))))
 
 (defn migrate-schema [conn]
   (let [datomic-schema (hodur-datomic/schema meta-db)]
@@ -39,42 +19,3 @@
 (comment
   (migrate-schema (d/connect (d/client {:server-type :dev-local
                           :system "dev"}) {:db-name "shopping-cards"})))
-
-(defn migrate-to-datomic []
-  (let [client (d/client {:server-type :dev-local
-                          :system "dev"})]
-    (d/delete-database client {:db-name "shopping-cards"})
-    (d/create-database client {:db-name "shopping-cards"})
-    (let [conn (d/connect client {:db-name "shopping-cards"})]
-      (migrate-schema conn)
-
-
-      (d/transact conn {:tx-data
-                        (map (fn [{:keys [id name type link image]}]
-                               (cond-> {:recipe/id id
-                                        :recipe/name name
-                                        :recipe/type (keyword (str "recipe-type/" (str/lower-case type)))
-                                        :recipe/image image}
-                                 link
-                                 (assoc :recipe/link link)))
-                             (load-recipes))})
-      (d/transact conn {:tx-data
-                        (map (fn [{:keys [id category name]}]
-                               {:ingredient/id id
-                                :ingredient/name name
-                                :ingredient/category (keyword (str "ingredient-category/" (str/replace (str/lower-case category) " " "")))})
-                             (load-ingredients))})
-
-      (d/transact conn {:tx-data
-                        (map (fn [{:keys [id ingredient-id recipe-id unit amount-desc amount]}]
-                               (cond->
-                                   {:cooked-with/id id
-                                    :cooked-with/ingredient [:ingredient/name (ingredient-name ingredient-id)]
-                                    :cooked-with/recipe [:recipe/name (recipe-name recipe-id)]}
-                                 ((complement str/blank?) amount-desc)
-                                 (assoc :cooked-with/amount-desc amount-desc)
-                                 unit
-                                 (assoc :cooked-with/unit unit)
-                                 amount
-                                 (assoc :cooked-with/amount (if (nil? amount) nil (float amount)))))
-                             (load-cooked-with))}))))
