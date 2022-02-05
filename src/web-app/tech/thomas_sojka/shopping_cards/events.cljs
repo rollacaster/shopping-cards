b321e718cc3233b60cb502b977fe298e7c400694
Thomas Sojka <thsojka@web.de>
2022-02-05 16:05:35 +0100

    Refactor: Move main to main.cljs

--- src/web-app/tech/thomas_sojka/shopping_cards/events.cljs
+++ src/web-app/tech/thomas_sojka/shopping_cards/events.cljs
@@ -13,7 +13,8 @@
             reg-global-interceptor]]
    [reagent.core :as r]
    [reitit.frontend.easy :as rfe]
-   [tech.thomas-sojka.shopping-cards.db :refer [default-db]]))
+   [tech.thomas-sojka.shopping-cards.db :refer [default-db]]
+   [tech.thomas-sojka.shopping-cards.main]))
 
 (defn check-and-throw
   "Throws an exception if `db` doesn't match the Spec `a-spec`."
@@ -68,152 +69,7 @@
                (dispatch event))
              time)))))
 
-(reg-event-fx
- :main/load-recipes
- (fn [{:keys [db]}]
-   (if (empty? (:main/recipes db))
-     {:db (assoc db :app/loading true)
-      :http-xhrio {:method :get
-                   :uri "recipes"
-                   :response-format (ajax/json-response-format {:keywords? true})
-                   :on-success [:main/success-recipes]
-                   :on-failure [:main/failure-recipes]}}
-     {:db db})))
 
-(reg-event-db
- :main/success-recipes
- (fn [db [_ data]]
-   (-> db
-       (assoc :app/loading false)
-       (assoc :main/recipes data))))
-
-(reg-event-db
- :main/failure-recipes
- (fn [db _]
-   (-> db
-       (assoc :app/loading false)
-       (assoc :main/recipes :ERROR))))
-
-
-(defn meal-plans-loaded-for-month? [db month-idx]
-  (->> (:main/meal-plans db)
-       (map #(.getMonth (:date %)))
-       (some #(= month-idx %))))
-
-(reg-event-fx
- :main/init-meal-plans
- (fn [{:keys [db]} [_ start-of-week]]
-   ;; TODO Load meals for all visible days
-   (if (meal-plans-loaded-for-month? db (.getMonth start-of-week))
-     {:db (assoc db :main/start-of-week start-of-week)}
-     {:db (-> db
-              (assoc :app/loading true)
-              (assoc :main/start-of-week start-of-week))
-      :http-xhrio {:method :get
-                   :uri (str "/meal-plans/" (inc (.getMonth start-of-week)))
-                   :response-format (ajax/json-response-format {:keywords? true})
-                   :on-success [:main/success-meal-plans]
-                   :on-failure [:main/failure-meal-plans]}})))
-
-(reg-event-db
- :main/success-meal-plans
- (fn [db [_ data]]
-   (-> db
-       (assoc :app/loading false)
-       (update :main/meal-plans
-               concat
-               (map (fn [meal-plan]
-                      (-> meal-plan
-                          (update :type keyword)
-                          (update :date #(js/Date. %))))
-                    data)))))
-
-(reg-event-db
- :main/failure-meal-plans
- (fn [db _]
-   (-> db
-       (assoc :app/loading false)
-       (assoc :main/meal-plans :ERROR))))
-
-(reg-event-fx
- :main/add-meal
- (fn [{:keys [db]} [_ recipe]]
-   {:db (-> db
-            (update :main/meal-plans conj (assoc (:recipe-details/meal db) :recipe recipe))
-            (assoc :recipe-details/meal nil))
-    :app/push-state [:tech.thomas-sojka.shopping-cards.view/meal-plan]
-    :http-xhrio {:method :post
-                 :uri "/meal-plans"
-                 :params (assoc (:recipe-details/meal db) :recipe recipe)
-                 :format (ajax/json-request-format)
-                 :response-format (ajax/text-response-format)
-                 :on-failure [:main/failure-add-meal (:recipe-details/meal db)]}}))
-
-(reg-event-db
- :main/success-bank-holidays
- (fn [db [_ data]]
-  (assoc db :main/bank-holidays (read-string data))))
-
-(reg-event-db
- :main/failure-bank-holidays
- (fn [db _]
-    ;; TODO Handle failed bank holiday loading
-   db))
-
-(defn remove-meal [meal-plans {:keys [date type]}]
-  (remove (fn [m] (and (= date (:date m))
-                       (= type (:type m))))
-          meal-plans))
-
-(reg-event-fx
- :main/failure-add-meal
- (fn [{:keys [db]} [_ failed-meal]]
-   {:db (-> db
-            (update :main/meal-plans remove-meal failed-meal)
-            (assoc :app/error "Fehler: Speichern fehlgeschlagen"))
-    :app/timeout {:id :app/error-removal
-                  :event [:app/remove-error]
-                  :time 2000}}))
-
-(reg-event-fx
- :main/select-meal
- (fn [{:keys [db]} [_ meal]]
-   {:app/push-state
-    (if (= (:type meal) :meal-type/lunch)
-      [:tech.thomas-sojka.shopping-cards.view/select-lunch]
-      [:tech.thomas-sojka.shopping-cards.view/select-dinner])
-    :db (assoc db :recipe-details/meal meal)}))
-
-(reg-event-fx
- :main/restart
- (fn [{:keys [db]} _]
-   {:app/push-state [:tech.thomas-sojka.shopping-cards.view/main]
-    :db (assoc db :shopping-card/selected-ingredient-ids #{})}))
-
-(reg-event-fx
- :main/remove-meal
- (fn [{:keys [db]}]
-   (let [{:keys [date type]} (:recipe-details/meal db)]
-     {:db
-      (update db :main/meal-plans remove-meal (:recipe-details/meal db))
-      :app/push-state [:tech.thomas-sojka.shopping-cards.view/meal-plan]
-      :http-xhrio {:method :delete
-                   :uri "/meal-plans"
-                   :url-params {:date (.toISOString date) :type type}
-                   :format (ajax/json-request-format)
-                   :response-format (ajax/text-response-format)
-                   :on-failure [:main/failure-remove-meal (:recipe-details/meal db)]}})))
-
-(reg-event-fx
- :main/failure-remove-meal
- (fn [{:keys [db]} [_ failed-meal]]
-   {:db
-    (-> db
-        (update :main/meal-plans conj failed-meal)
-        (assoc :app/error "Fehler: Löschen fehlgeschlagen"))
-    :app/timeout {:id :app/error-removal
-                  :event [:app/remove-error]
-                  :time 2000}}))
 
 (defn toggle-map [key map]
   ((if (map key) disj conj) map key))
@@ -303,12 +159,12 @@
             ;; TODO update to has shopping-list
             (update :main/meal-plans #(map (fn [meal-plan]
                                              (if
-                                              (some
-                                               (fn [meal-with-shopping-list]
-                                                 (and
-                                                  (= (:type meal-with-shopping-list) (:type meal-plan))
-                                                  (= (:date meal-with-shopping-list) (:date meal-plan))))
-                                               meals-without-shopping-list)
+                                                 (some
+                                                  (fn [meal-with-shopping-list]
+                                                    (and
+                                                     (= (:type meal-with-shopping-list) (:type meal-plan))
+                                                     (= (:date meal-with-shopping-list) (:date meal-plan))))
+                                                  meals-without-shopping-list)
                                                (assoc meal-plan :shopping-list true)
                                                meal-plan))
                                            (:main/meal-plans db)))
--- src/web-app/tech/thomas_sojka/shopping_cards/view.cljs
+++ src/web-app/tech/thomas_sojka/shopping_cards/view.cljs
@@ -1,22 +1,7 @@
 (ns tech.thomas-sojka.shopping-cards.view
   (:require [re-frame.core :refer [dispatch subscribe]]
-            [reagent.core :as r]
-            ["date-fns" :refer (format subDays startOfDay addDays isPast)]
-            ["date-fns/locale" :refer (de)]))
-
-(def icons {:check-mark "M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"
-            :trash-can "M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"
-            :shopping-cart "m 18.595802,0.56054688 c -0.459356,0 -0.816128,0.30784707 -0.967618,0.71344852 L 13.552604,14.165318 H 4.8395597 L 2.3961775,6.9817746 h 9.1724185 c 0.561978,0 1.021353,-0.4593749 1.021353,-1.0213529 0,-0.561978 -0.459375,-1.021353 -1.021353,-1.021353 H 1.0180526 C 0.11400118,4.9830496 -0.11561948,5.8187628 0.05053051,6.2145907 L 3.1584936,15.484745 c 0.1514897,0.405601 0.5620161,0.664581 0.9676175,0.664581 H 14.266147 c 0.459356,0 0.864901,-0.254093 0.967523,-0.664581 L 19.30925,2.5934218 h 3.669941 c 0.561978,0 1.021353,-0.4592795 1.021353,-1.0212574 0,-0.5619779 -0.464262,-1.01161752 -1.021353,-1.01161752 z M 5.0447658,17.98673 c -1.7836692,0 -3.2594339,1.47586 -3.2594339,3.259529 0,1.783669 1.4757647,3.259434 3.2594339,3.259434 1.783669,0 3.259529,-1.475765 3.259529,-3.259434 0,-1.783669 -1.47586,-3.259529 -3.259529,-3.259529 z m 7.9458602,0 c -1.783669,0 -3.2594341,1.47586 -3.2594341,3.259529 0,1.783669 1.4757651,3.259434 3.2594341,3.259434 1.832536,0 3.259529,-1.475765 3.259529,-3.259434 0.0049,-1.783669 -1.47586,-3.259529 -3.259529,-3.259529 z M 5.0447658,20.0197 c 0.7134676,0.0049 1.2754455,0.561959 1.2216911,1.275427 0,0.713467 -0.5619779,1.275426 -1.2216911,1.275426 -0.6646002,0 -1.2216913,-0.561959 -1.2216913,-1.275426 0,-0.713468 0.5619779,-1.275427 1.2216913,-1.275427 z m 7.9508232,0 c 0.708581,0.0049 1.270558,0.561959 1.221691,1.275427 0,0.713467 -0.561978,1.275426 -1.221691,1.275426 -0.659713,0 -1.221691,-0.561959 -1.221691,-1.275426 0,-0.713468 0.561978,-1.275427 1.221691,-1.275427 z"
-            :add "M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"
-            :remove "M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z"
-            :back "M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"})
-
-(defn icon
-  ([name]
-   [icon {} name])
-  ([{:keys [class style]} name]
-   [:svg {:viewBox "0 0 24 24" :class class :style style}
-    [:path {:d (icons name) :fill "currentColor"}]]))
+            [tech.thomas-sojka.shopping-cards.components :as c]
+            [tech.thomas-sojka.shopping-cards.main :as main]))
 
 (defn recipe [{:keys [even name image selected? on-click]}]
   [:button.relative.w-100.w-auto-ns.flex.db-ns.tl.outline-transparent.bg-trbg-gray-600-ns.white-ns.pa0.bt-0.br-0.bl-0.bb-0-ns.bb.b--gray-900.bw1.ml3-ns.mb3-ns.br2-ns.h3.h-auto-ns
@@ -26,10 +11,10 @@
       [:<>
        [:div.w3.h3.absolute.orange-400.db.dn-ns
         {:style {:top "50%" :left "2%" :transform "translate(0,-50%)"}}
-        [icon :check-mark]]
+        [c/icon :check-mark]]
        [:div.w4.h4.absolute.white.dn.db-ns
-       {:style {:top "50%" :left "50%" :transform "translate(-50%,-50%)"}}
-       [icon :check-mark]]])
+        {:style {:top "50%" :left "50%" :transform "translate(-50%,-50%)"}}
+        [c/icon :check-mark]]])
     [:img.br2-ns.w-100.h-100 {:style {:object-fit "cover"} :src image :class (when selected? "o-40")}]]
    [:div.bg-gray-700.absolute.pa2.mh2.mb2.bottom-0.o-50.br2.dn.db-ns
     [:span.f4 name]]
@@ -113,7 +98,7 @@
         (when-not shopping-list
           [:button.pv2.br3.bg-orange-200.bn.shadow-2.self-start
            {:on-click #(dispatch [:main/remove-meal])}
-           [icon {:class "dark-gray h2"} :trash-can]])]
+           [c/icon {:class "dark-gray h2"} :trash-can]])]
        [:div.flex.justify-between.flex-wrap
         [:div.bw1.w-50-ns.order-1-ns.flex.justify-center-ns.h-100
          [:img.w5.br3.ba.b--orange-300 {:src image}]]
@@ -135,34 +120,6 @@
       [:div.pv3.ph5-ns.ph3.flex.justify-between.w-100.items-center
        [:h1.ma0.gray-800.ml2-ns.truncate
         (:title (:data route))]]]]))
-
-(defn spinner []
-  [:svg {:width 38 :height 38
-         :viewBox "0 0 100 100"
-         :style {:transform "scale(1.8)"}
-         :preserveAspectRatio "xMidYMid"}
-   [:g
-    [:circle {:cx "78.0502" :cy "50" :r "4" :fill "#e15b64"} [:animate {:attributeName "cx" :repeatCount "indefinite" :dur "1s" :values "95;35" :keyTimes "0;1" :begin "-0.67s"}] [:animate {:attributeName "fill-opacity" :repeatCount "indefinite" :dur "1s" :values "0;1;1" :keyTimes "0;0.2;1" :begin "-0.67s"}]]
-    [:circle {:cx "38.4502" :cy "50" :r "4" :fill "#e15b64"} [:animate {:attributeName "cx" :repeatCount "indefinite" :dur "1s" :values "95;35" :keyTimes "0;1" :begin "-0.33s"}] [:animate {:attributeName "fill-opacity" :repeatCount "indefinite" :dur "1s" :values "0;1;1" :keyTimes "0;0.2;1" :begin "-0.33s"}]]
-    [:circle {:cx "58.2502" :cy "50" :r "4" :fill "#e15b64"} [:animate {:attributeName "cx" :repeatCount "indefinite" :dur "1s" :values "95;35" :keyTimes "0;1" :begin "0s"}] [:animate {:attributeName "fill-opacity" :repeatCount "indefinite" :dur "1s" :values "0;1;1" :keyTimes "0;0.2;1" :begin "0s"}]]]
-   [:g {:transform "translate(-15 0)"}
-    [:path {:d "M50 50L20 50A30 30 0 0 0 80 50Z" :fill "#f8b26a" :transform "rotate(90 50 50)"}]
-    [:path {:d "M50 50L20 50A30 30 0 0 0 80 50Z" :fill "#f8b26a" :transform "rotate(34.8753 50 50)"}
-     [:animateTransform {:attributeName "transform" :type "rotate" :repeatCount "indefinite" :dur "1s" :values "0 50 50;45 50 50;0 50 50" :keyTimes "0;0.5;1"}]]
-    [:path {:d "M50 50L20 50A30 30 0 0 1 80 50Z" :fill "#f8b26a" :transform "rotate(-34.8753 50 50)"}
-     [:animateTransform {:attributeName "transform" :type "rotate" :repeatCount "indefinite" :dur "1s" :values "0 50 50;-45 50 50;0 50 50" :keyTimes "0;0.5;1"}]]]])
-
-(defn footer [{:keys [on-click loading]}]
-  [:footer.bg-orange-400.flex.justify-center.pa3
-   [:button.br3.bg-gray-700.pointer.bn.shadow-3.ph3.pv2.white
-    {:on-click on-click}
-    [:div.flex.items-center
-     (if loading
-       [:div {:style {:width 128}}
-        [spinner]]
-       [:<>
-        [:span.f2.mr2 "Fertig!"]
-        [:span.w2.h2.pt1 [icon {:color "white"} :check-mark]]])]]])
 
 (defn app []
   (fn []
@@ -202,81 +159,12 @@
        [ingredient {:i (count ingredients) :id "add-ingredient" :class "ba b--dashed"}
         [:div.flex.items-center
          [:div.w2.flex.items-center.mr3
-          [icon :add]]
+          [c/icon :add]]
          "Zutat hinzufügen"]]]]
      [:div.fixed.bottom-0.w-100.z-2
-      [footer {:on-click #(dispatch [:shopping-card/create
-                                     meals-without-shopping-list])
-               :app/loading loading}]]]))
-
-(defn meal-name [meal-plan]
-  (if (:recipe meal-plan)
-    (:name (:recipe meal-plan))
-    (case (:type meal-plan)
-      :meal-type/lunch "Mittagessen"
-      :meal-type/dinner "Abendessen")))
-
-(defn meal [meal-plan]
-  (let [has-recipe? (:recipe meal-plan)]
-    [:button.pt2.ph2.h-50.bg-transparent.bn.w-100.relative
-     {:on-click #(dispatch
-                  (if has-recipe?
-                    [:recipe-details/show-meal-details meal-plan]
-                    [:main/select-meal meal-plan]))}
-     [:div.h-100.br3.bg-center.cover.relative
-      {:style {:background-image (if has-recipe? (str "url(" (:image (:recipe meal-plan)) ")") "")}}
-      (when has-recipe? [:div.o-40.bg-orange.absolute.h-100.w-100.br3])
-      [:h4.f4.fw5.mv0.br3.h-100.bw1.overflow-hidden.flex.justify-center.items-center.absolute.w-100
-       {:class (r/class-names (if has-recipe? "white" "ba b--gray b--dashed gray"))}
-       (when (:shopping-list meal-plan)
-         [:div.absolute.bottom-0.right-0.mr1.bg-orange-400.br-100
-          {:style {:width "1.8rem" :padding 5}}
-          [icon :shopping-cart]])
-       (meal-name meal-plan)]]]))
-
-(defn meal-plan []
-  (dispatch [:main/load-recipes])
-  (dispatch [:main/init-meal-plans (js/Date.)])
-  (fn []
-    (let [meals-plans @(subscribe [:main/weekly-meal-plans])
-          start-of-week @(subscribe [:main/start-of-week])
-          meals-without-shopping-list @(subscribe [:main/meals-without-shopping-list])]
-      [:div.ph5-ns.flex.flex-column.h-100
-       [:div.flex.items-center.justify-between
-        [:div.pv2.flex
-         [:button.pv2.w3.bg-gray-600.ba.br3.br--left.white.b--white.tc.flex.justify-center
-          {:on-click
-           #(dispatch [:main/init-meal-plans (startOfDay (js/Date.))])}
-          "Heute"]
-         [:button.pv2.w3.bg-gray-600.ba.bl-0.br-0.white.b--white.tc.flex.justify-center
-          {:on-click
-           #(dispatch [:main/init-meal-plans (subDays (startOfDay start-of-week) 4)])}
-          "Zurück"]
-         [:button.pv2.w3.bg-gray-600.ba.br3.br--right.white.b--white.tc.flex.justify-center
-          {:on-click
-           #(dispatch [:main/init-meal-plans (addDays (startOfDay start-of-week) 4)])}
-          "Vor"]]
-        [:div.flex.justify-center.flex-auto
-         (format (:date (ffirst meals-plans)) "MMMM yyyy" #js {:locale de})]]
-       [:div.flex.flex-wrap.flex-auto
-        (doall
-         (map
-          (fn [[lunch dinner]]
-            (let [bank-holiday @(subscribe [:main/bank-holiday (:date lunch)])]
-              ^{:key (:date lunch)}
-              [:div.ba.w-50.pv2.flex.flex-column.b--gray.h-50
-               [:div.ph2.flex.justify-between
-                [:span.truncate.dark-red bank-holiday]
-                [:span.tr.fw6
-                 {:style {:white-space "nowrap"}}
-                 (format (:date lunch) "EEEEEE dd.MM" #js {:locale de})]]
-               [:div.flex-auto
-                {:class (when (isPast (addDays (startOfDay start-of-week) 2)) "o-20")}
-                [meal lunch]
-                [meal dinner]]]))
-          meals-plans))]
-       (when (seq meals-without-shopping-list)
-         [footer {:on-click #(dispatch [:shopping-card/load-ingredients-for-meals meals-without-shopping-list])}])])))
+      [c/footer {:on-click #(dispatch [:shopping-card/create
+                                       meals-without-shopping-list])
+                 :app/loading loading}]]]))
 
 (defn add-ingredients []
   (let [ingredients @(subscribe [:extra-ingredients/addable-ingredients])
@@ -298,38 +186,34 @@
                         :selected? false}
                        [:div.flex.w-100
                         [:div.mr3.flex.items-center
-                         [icon {:style {:width 20}} :add]]
+                         [c/icon {:style {:width 20}} :add]]
                         name]]])
                    ingredients)]
      (when (seq [])
-       [footer {:on-click #(dispatch [:add-ingredients []])}])]))
+       [c/footer {:on-click #(dispatch [:add-ingredients []])}])]))
 
 (def routes
-  [["/" {:name ::main
-         :view meal-plan
-         :title "Essensplan"}]
-   ["/meal-plan-details"
-    {:name ::meal-plan-details
-     :view meal-plan-details
-     :title "Rezept"}]
-   ["/deselect-ingredients" {:name ::deselect-ingredients
-                             :view deselect-ingredients
-                             :title "Zutaten auswählen"}]
-   ["/add-ingredients" {:name ::add-ingredients
-                        :view add-ingredients
-                        :title "Zutaten hinzufügen"}]
-   ["/finish/:card-id" {:name ::finish
-                        :view finish
-                        :title "Einkaufszettel erstellt"
-                        :parameters {:path {:card-id string?}}}]
-   ["/meal-plan" {:name ::meal-plan
-                  :view meal-plan
-                  :title "Essensplan"}]
-   ["/select-lunch"
-    {:name ::select-lunch
-     :view select-lunch
-     :title "Mittag auswählen"}]
-   ["/select-dinner"
-    {:name ::select-dinner
-     :view select-dinner
-     :title "Abendessen auswählen"}]])
+  (concat
+   main/routes
+   [["/meal-plan-details"
+     {:name ::meal-plan-details
+      :view meal-plan-details
+      :title "Rezept"}]
+    ["/deselect-ingredients" {:name ::deselect-ingredients
+                              :view deselect-ingredients
+                              :title "Zutaten auswählen"}]
+    ["/add-ingredients" {:name ::add-ingredients
+                         :view add-ingredients
+                         :title "Zutaten hinzufügen"}]
+    ["/finish/:card-id" {:name ::finish
+                         :view finish
+                         :title "Einkaufszettel erstellt"
+                         :parameters {:path {:card-id string?}}}]
+    ["/select-lunch"
+     {:name ::select-lunch
+      :view select-lunch
+      :title "Mittag auswählen"}]
+    ["/select-dinner"
+     {:name ::select-dinner
+      :view select-dinner
+      :title "Abendessen auswählen"}]]))
