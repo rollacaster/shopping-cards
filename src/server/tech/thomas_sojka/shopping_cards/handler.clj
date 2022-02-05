2c5da468959244af80fc7e92a6aa24ba8210e673
Thomas Sojka <thsojka@web.de>
2022-02-05 11:42:47 +0100

    Run linter

--- src/server/tech/thomas_sojka/shopping_cards/handler.clj
+++ src/server/tech/thomas_sojka/shopping_cards/handler.clj
@@ -1,4 +1,7 @@
 (ns tech.thomas-sojka.shopping-cards.handler
+  {:clj-kondo/config '{:linters {:unresolved-symbol {:exclude [(compojure.api.sweet/GET)
+                                                               (compojure.api.sweet/POST)
+                                                               (compojure.api.sweet/DELETE)]}}}}
   (:require [compojure.api.sweet :refer [api GET POST DELETE]]
             [compojure.route :as route]
             [muuntaja.middleware :refer [wrap-format]]
@@ -18,7 +21,7 @@
    (GET "/" [] (resource-response "index.html" {:root "public"}))
    (GET "/recipes" [] {:status 200 :body (vec (filter (comp not :inactive) (load-recipes))) :headers {"Content-type" "application/edn"}})
    (GET "/recipes/:recipe-id/ingredients" [recipe-id]
-        (pr-str (ingredients-for-recipe recipe-id)))
+     (pr-str (ingredients-for-recipe recipe-id)))
    (GET "/ingredients" [recipe-ids]
      (if recipe-ids
        (pr-str (ingredients-for-recipes ((if (vector? recipe-ids) set hash-set) recipe-ids)))
@@ -26,19 +29,19 @@
         :body (load-ingredients)
         :headers {"Content-type" "application/edn"}}))
    (POST "/shopping-card" request
-         {:status 201
-          :body (create-klaka-shopping-card (:body-params request))
-          :headers {"Content-type" "application/edn"}})
+     {:status 201
+      :body (create-klaka-shopping-card (:body-params request))
+      :headers {"Content-type" "application/edn"}})
    (GET "/meal-plans/:month" [month]
      {:status 200
       :body (load-meal-plans month)
       :headers {"Content-type" "application/edn"}})
    (POST "/meal-plans" request
      (let [{:keys [date recipe type]} (:body-params request)]
-          (create-meal-plan
-           {:inst (read-instant-date date)
-            :type (case type "lunch" :meal-type/lunch "dinner" :meal-type/dinner)
-            :recipe [:recipe/name (:name recipe)]}))
+       (create-meal-plan
+        {:inst (read-instant-date date)
+         :type (case type "lunch" :meal-type/lunch "dinner" :meal-type/dinner)
+         :recipe [:recipe/name (:name recipe)]}))
      {:status 200})
    (DELETE "/meal-plans" [date type]
      (delete-meal-plan {:date (read-instant-date date)
