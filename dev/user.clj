9a7b3d5b33797a8acc97270e8869c8177bc291c5
Thomas Sojka <thsojka@web.de>
2022-04-18 14:34:02 +0200

    Update shadow-cljs setup

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,8 @@
 (ns user
-  (:require [integrant.repl :as ig-repl]
-            [integrant.core :as ig]
+  (:require [integrant.core :as ig]
+            [integrant.repl :as ig-repl]
+            [shadow.cljs.devtools.api :as shadow]
+            [shadow.cljs.devtools.server :as server]
             [tech.thomas-sojka.shopping-cards.system :as system]))
 
 (ig-repl/set-prep! (fn [] system/config))
@@ -9,6 +11,16 @@
   {:create-klaka-shopping-card (fn [ingredients]
                                  (prn "New Card" ingredients)
                                  "mock")})
+
+(defn cljs-repl
+  "Connects to a given build-id. Defaults to `:app`."
+  ([]
+   (cljs-repl :app))
+  ([build-id]
+   (server/start!)
+   (shadow/watch build-id)
+   (shadow/nrepl-select build-id)))
+
 (ig-repl/go)
 (comment
   (ig-repl/go)
