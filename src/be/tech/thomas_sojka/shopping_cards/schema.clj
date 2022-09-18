547376e4b99fd69e3084fb6084a12989ab4f8333
Thomas Sojka <thsojka@web.de>
2022-09-18 17:10:30 +0200

    Add snippet to visualize schema

--- src/be/tech/thomas_sojka/shopping_cards/schema.clj
+++ src/be/tech/thomas_sojka/shopping_cards/schema.clj
@@ -1,5 +1,6 @@
 (ns tech.thomas-sojka.shopping-cards.schema
-  (:require [hodur-engine.core :as hodur]))
+  (:require [hodur-engine.core :as hodur]
+            [hodur-graphviz-schema.core :as hodur-graphviz]))
 
 (def meta-db
   (hodur/init-schema
@@ -44,12 +45,19 @@
      MealType
      [DINNER LUNCH]
 
+     ^{:graphviz/tag-recursive true}
      MealPlan
      [^{:type String :datomic/unique :db.unique/identity} id
       ^DateTime inst
       ^MealType type
       ^Recipe recipe]
 
+     ^{:graphviz/tag-recursive true}
      ShoppingList
      [^{:type MealPlan :cardinality [1 n]}
       meals]]))
+
+(comment
+  (do
+    (spit "resources/schema.dot" (hodur-graphviz/schema meta-db))
+    (clojure.java.shell/sh "dot" "-Tpng" "-oresources/schema.png" "resources/schema.dot")))
