0bd218f049eddda1e2785e889169b12e213a2f95
Thomas Sojka <thsojka@web.de>
2023-02-14 08:12:25 +0100

    Add firestore migration scripts

--- src/fe/tech/thomas_sojka/shopping_cards/firestore.cljs
+++ src/fe/tech/thomas_sojka/shopping_cards/firestore.cljs
@@ -7,6 +7,56 @@
 
 (def db (.getFirestore firestore firebase/app))
 
+(defn add-recipe [recipe]
+  (firestore/setDoc (firestore/doc (firestore/collection db "recipes") (:recipe/id recipe))
+                    (clj->js recipe)))
+
+(defn add-ingredient [ingredient]
+  (firestore/setDoc (firestore/doc (firestore/collection db "ingredients") (:ingredient/id ingredient))
+                    (clj->js ingredient)))
+
+(defn log-recipes []
+  (-> (firestore/getDocs (firestore/collection db "recipes"))
+      (.then (fn [snapshot]
+               (let [datoms-snapshot (volatile! [])]
+                 (.forEach snapshot
+                           (fn [doc]
+                             (vswap! datoms-snapshot conj (-> doc
+                                                              .data
+                                                              (js->clj :keywordize-keys true)))))
+                 (js/console.log @datoms-snapshot) )))))
+
+(defn migrate-recipes []
+  (doseq [[recipe] (d/q
+                    '[:find (pull ?r
+                                  [[:recipe/id]
+                                   [:recipe/name]
+                                   [:recipe/image]
+                                   [:recipe/link]
+                                   [:recipe/type]
+                                   {[:cooked-with/_recipe :as :ingredients]
+                                    [[:cooked-with/amount]
+                                     [:cooked-with/unit]
+                                     [:cooked-with/amount-desc]
+                                     {[:cooked-with/ingredient]
+                                      [[:ingredient/name]
+                                       [:ingredient/id]
+                                       [:ingredient/category]]}]}])
+                      :where [?r :recipe/id]]
+                    @db/conn)]
+    (add-recipe recipe)))
+
+(defn migrate-ingredients []
+  (doseq [[ingredient] (d/q
+                        '[:find (pull ?i
+                                      [[:ingredient/name]
+                                       [:ingredient/id]
+                                       [:ingredient/category]])
+                          :where [?i :ingredient/id]]
+                        @db/conn)]
+    (add-ingredient ingredient)))
+
+
 (reg-fx :firestore/doc
   (fn [{:keys [path key on-success on-failure]}]
     (-> (firestore/getDoc (firestore/doc db path key))
