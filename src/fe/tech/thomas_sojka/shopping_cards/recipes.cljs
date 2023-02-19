a5c8b5d5160dba4cbd661227dabbb96cc779f4d8
Thomas Sojka <thsojka@web.de>
2023-02-19 10:42:42 +0100

    Cleanup recipes

--- src/fe/tech/thomas_sojka/shopping_cards/recipes.cljs
+++ src/fe/tech/thomas_sojka/shopping_cards/recipes.cljs
@@ -3,87 +3,6 @@
             [re-frame.core :refer [reg-event-db reg-event-fx reg-sub]]
             [tech.thomas-sojka.shopping-cards.ingredients :as ingredients]))
 
-(reg-sub :recipes
- (fn [db _]
-   (:recipes db)))
-
-(reg-sub :recipes/details
-  :<- [:recipes]
-  (fn [recipes [_ recipe-id]]
-    (some
-     (fn [{:keys [id] :as recipe}] (when (= id recipe-id) recipe))
-     recipes)))
-
-(reg-sub :recipes/recipe-types
- :<- [:recipes]
- (fn [recipes]
-   (set (map :recipe/type recipes))))
-
-(defn sort-recipes [type-order recipes]
-  (sort-by
-   (fn [[recipe-type]] (->> type-order
-                           (map-indexed #(vector %1 %2))
-                           (some
-                            (fn [[idx recipe-type-order]]
-                              (when (= recipe-type-order recipe-type) idx)))))
-   recipes))
-
-(defn lunch-recipes [recipes]
-  (->> recipes
-       (group-by :recipe/type)
-       (sort-recipes [:recipe-type/fast :recipe-type/new :recipe-type/normal :recipe-type/misc  :recipe-type/rare])))
-
-(reg-sub :recipes/lunch
- :<- [:recipes]
- lunch-recipes)
-
-(defn dinner-recipes [recipes]
-  (->> recipes
-       (group-by :recipe/type)
-       (sort-recipes [:recipe-type/normal :recipe-type/fast :recipe-type/new :recipe-type/misc  :recipe-type/rare])))
-
-(reg-sub :recipes/dinner
- :<- [:recipes]
- dinner-recipes)
-
-(reg-event-fx :recipes/add
-  (fn [_ [_ recipe]]
-    (let [new-id (str (random-uuid))]
-      {:firestore/add-doc {:path "recipes"
-                           :data (assoc recipe :id new-id)
-                           :on-success [:recipes/add]
-                           :on-failure [:recipes/add-failure]}})))
-
-(reg-event-fx :recipes/add-success
-  (fn []
-    {:app/push-state [:route/main]}))
-
-(reg-event-fx :recipes/add-failure
- (fn [{:keys [db]}]
-   {:db (assoc db :app/error "Fehler: Speichern fehlgeschlagen")
-    :app/timeout {:id :app/error-removal
-                  :event [:app/remove-error]
-                  :time 2000}}))
-
-(reg-event-fx :recipes/remove
-  (fn [_ [_ recipe-id]]
-    (def recipe-id ){:firestore/remove-doc {:path "(first reciprecipees"
-                              :key recipe-id
-                              :on-success [:recipes/remove-success]
-                              :on-failure [:recipes/remove-failure]}}))
-
-(reg-event-fx :recipes/remove-success
-  (fn []
-    {:app/push-state [:route/recipe]}))
-
-(reg-event-fx :recipes/remove-failure
- (fn [{:keys [db]}]
-   {:db (assoc db :app/error "Fehler: Löschen fehlgeschlagen")
-    :app/timeout {:id :app/error-removal
-                  :event [:app/remove-error]
-                  :time 2000}}))
-
-
 (reg-event-fx :recipes/load
   (fn []
     {:firestore/snapshot {:path "recipes"
@@ -93,7 +12,7 @@
 (defn ->recipe [firestore-recipe]
   (cond-> firestore-recipe
       :always (set/rename-keys {:type :recipe/type})
-      :always(update :recipe/type (fn [t] (keyword "recipe-type" t)))
+      :always (update :recipe/type (fn [t] (keyword "recipe-type" t)))
       (:ingredients firestore-recipe)
       (update :ingredients (fn [cooked-with]
                              (map
@@ -110,6 +29,44 @@
  (fn [db [_ data]]
    (assoc db :recipes (map ->recipe data))))
 
-(reg-event-db :main/failure-recipes
+(reg-event-db :recipes/load-failure
  (fn [db _]
    (assoc db :recipes :ERROR)))
+
+(reg-sub :recipes
+ (fn [db _]
+   (:recipes db)))
+
+(reg-sub :recipes/details
+  :<- [:recipes]
+  (fn [recipes [_ recipe-id]]
+    (some
+     (fn [{:keys [id] :as recipe}] (when (= id recipe-id) recipe))
+     recipes)))
+
+(reg-sub :recipes/recipe-types
+ :<- [:recipes]
+ (fn [recipes]
+   (set (map :recipe/type recipes))))
+
+(defn group-recipes [order recipes]
+  (->> recipes
+       (group-by :recipe/type)
+       (sort-by
+        (fn [[recipe-type]] (->> order
+                                (map-indexed #(vector %1 %2))
+                                (some
+                                 (fn [[idx recipe-type-order]]
+                                   (when (= recipe-type-order recipe-type) idx))))))))
+
+(reg-sub :recipes/lunch
+ :<- [:recipes]
+  (fn [recipes]
+    (group-recipes [:recipe-type/fast :recipe-type/new :recipe-type/normal :recipe-type/misc  :recipe-type/rare]
+                   recipes)))
+
+(reg-sub :recipes/dinner
+ :<- [:recipes]
+  (fn [recipes]
+    (group-recipes [:recipe-type/normal :recipe-type/fast :recipe-type/new :recipe-type/misc  :recipe-type/rare]
+                   recipes)))
