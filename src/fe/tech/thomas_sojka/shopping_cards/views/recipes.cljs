be212ab170a4aa59972302a3bd9c17e687def44c
Thomas Sojka <thsojka@web.de>
2023-11-29 08:42:41 +0100

    Clean imports

--- src/fe/tech/thomas_sojka/shopping_cards/views/recipes.cljs
+++ src/fe/tech/thomas_sojka/shopping_cards/views/recipes.cljs
@@ -3,8 +3,7 @@
             [re-frame.core :refer [subscribe]]
             [reagent.core :as r]
             [reitit.frontend.easy :as rfe]
-            [tech.thomas-sojka.shopping-cards.components :as c]
-            [tech.thomas-sojka.shopping-cards.components :refer [icon]]))
+            [tech.thomas-sojka.shopping-cards.components :as c]))
 
 (defn recipe [{:keys [even name image selected? details-link]}]
   [:a.no-underline.relative.w-100.w-auto-ns.flex.db-ns.tl.outline-transparent.bg-gray-600-ns.pa0.bt-0.br-0.bl-0.bb-0-ns.bb.b--gray-900.bw1.ml3-ns.mb3-ns.br2-ns.h3.h-auto-ns
@@ -14,10 +13,10 @@
       [:<>
        [:div.w3.h3.absolute.orange-400.db.dn-ns
         {:style {:top "50%" :left "2%" :transform "translate(0,-50%)"}}
-        [icon :check-mark]]
+        [c/icon :check-mark]]
        [:div.w4.h4.absolute.white.dn.db-ns
         {:style {:top "50%" :left "50%" :transform "translate(-50%,-50%)"}}
-        [icon :check-mark]]])
+        [c/icon :check-mark]]])
     [:img.br2-ns.w-100.h-100 {:style {:object-fit "cover"} :src image :class (when selected? "o-40")}]]
    [:div.bg-gray-700.absolute.pa2.mh2.mb2.bottom-0.o-50.br2.dn.db-ns
     [:span.f4 name]]
