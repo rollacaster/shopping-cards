6e72d4c03845052ef024bae831da6153b03db98d
Thomas Sojka <thsojka@web.de>
2024-01-22 16:15:49 +0100

    Implement reorder

--- src/tech/thomas_sojka/shopping_cards/views/sort_ingredients.cljs
+++ src/tech/thomas_sojka/shopping_cards/views/sort_ingredients.cljs
@@ -3,36 +3,52 @@
             ["react-beautiful-dnd" :as react-beautiful-dnd]
             [reagent.core :as r]))
 
-(defn ingredient []
-  [:ul
-   (let [ingredients @(rf/subscribe [:ingredients])]
-     (->> ingredients
-          (map-indexed
-           (fn [index {:keys [:ingredient/name]}]
-             [:> react-beautiful-dnd/Draggable {:key name :draggableId name
-                                                :index index}
-              (fn [provided snapshot]
-                (let [{:keys [draggableProps dragHandleProps innerRef]} (js->clj provided :keywordize-keys true)]
-                  (r/as-element
-                   [:li.ba
-                    (assoc-in
-                     (merge
-                      {:ref innerRef}
-                      draggableProps
-                      dragHandleProps)
-                     [:style :height] 50)
-                    name])))]))))])
+
+(defn- reorder [items start-index end-index]
+  (if (< start-index end-index)
+    (reduce
+     into
+     [(subvec items 0 start-index)
+      (subvec items (inc start-index) (inc end-index))
+      [(get items start-index)]
+      (subvec items (inc end-index))])
+    (reduce
+     into
+     [(subvec items 0 end-index)
+      [(get items start-index)]
+      (subvec items end-index start-index)
+      (subvec items (inc start-index))])))
+
+(defn- sort-ingredients [{:keys [ingredients]}]
+  (let [ingredients (r/atom (vec ingredients))]
+    (fn []
+      [:> react-beautiful-dnd/DragDropContext {:onDragEnd (fn [result]
+                                                            (when (.-destination result)
+                                                              (swap! ingredients
+                                                                     #(reorder %
+                                                                               ^js (.-source.index result)
+                                                                               ^js (.-destination.index result)))))}
+       [:> react-beautiful-dnd/Droppable {:droppableId "droppable"}
+        (fn [provided snapshot]
+          (let [{:keys [droppableProps innerRef]} (js->clj provided :keywordize-keys true)]
+            (r/as-element
+             [:div (merge droppableProps {:ref innerRef})
+              [:ul.list.ph5
+               (->> @ingredients
+                    (map-indexed
+                     (fn [index {:keys [:ingredient/name]}]
+                       [:> react-beautiful-dnd/Draggable {:key name :draggableId name :index index}
+                        (fn [provided snapshot]
+                          (let [{:keys [draggableProps dragHandleProps innerRef]} (js->clj provided :keywordize-keys true)]
+                            (r/as-element
+                             [:li.ba.pa2.bg-orange-400.white.b.f3
+                              (assoc-in
+                               (merge {:ref innerRef} draggableProps dragHandleProps)
+                               [:style :height] 50)
+                              name])))])))]
+              (.-placeholder provided)])))]])))
 
 (defn main []
-  [:div
-   [:h1 "Sort Ingredients"]
-   [:> react-beautiful-dnd/DragDropContext {:onDragEnd (fn [e] (prn e))}
-    [:> react-beautiful-dnd/Droppable {:droppableId "droppable"}
-     (fn [provided snapshot]
-       (let [{:keys [droppableProps innerRef]} (js->clj provided :keywordize-keys true)]
-         (r/as-element
-          [:div
-           (merge droppableProps
-                  {:ref innerRef})
-           [ingredient]
-           (.-placeholder provided)])))]]])
+  (let [ingredients @(rf/subscribe [:ingredients])]
+    (when (seq ingredients)
+      [sort-ingredients {:ingredients ingredients}])))
