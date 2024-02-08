48787e58224551292b78aedcb6949c48e341a315
Thomas Sojka <thsojka@web.de>
2024-02-08 06:30:54 +0100

    Readd sorting

--- test/tech/thomas_sojka/shopping_cards/fixtures.cljc
+++ test/tech/thomas_sojka/shopping_cards/fixtures.cljc
@@ -1,15 +1,36 @@
-(ns tech.thomas-sojka.shopping-cards.fixtures)
+(ns tech.thomas-sojka.shopping-cards.fixtures
+  (:require [tech.thomas-sojka.shopping-cards.recipes :as recipes]))
 
 (def ingredients
-  [#:ingredient{:id "dc1b7bdc-9f9e-4751-b935-468919d39030",
-                :name "Carrot",
-                :category :ingredient-category/gemüse}
-   #:ingredient{:id "bbaba432-0330-4043-90c6-3d3df2fac57b",
+  [#:ingredient{:id "bbaba432-0330-4043-90c6-3d3df2fac57b",
                 :name "Onion",
+                :category :ingredient-category/gemüse}
+   #:ingredient{:id "dc1b7bdc-9f9e-4751-b935-468919d39030",
+                :name "Carrot",
                 :category :ingredient-category/gemüse}
    #:ingredient{:id "e0fb50e5-ec7f-4b80-9456-e9f9d2fbd68f",
                 :name "Mushroom",
+                :category :ingredient-category/gemüse}
+   #:ingredient{:id "6c0740a2-24a0-4aa7-9548-60c79bac6fec",
+                :name "Paprika",
                 :category :ingredient-category/gemüse}])
+
+(def shopping-items
+  [#:shopping-item{:ingredient-id "bbaba432-0330-4043-90c6-3d3df2fac57b",
+                   :id #uuid "3251db72-593f-4ea4-91b7-ea34e7359910",
+                   :status :open,
+                   :created-at #inst "2024-01-20T12:35:00.340-00:00",
+                   :content "Onion"}
+   #:shopping-item{:ingredient-id "dc1b7bdc-9f9e-4751-b935-468919d39030",
+                   :id #uuid "475c210a-f804-4f77-ba71-05ea93da73d7",
+                   :status :open,
+                   :created-at #inst "2024-01-20T12:35:00.340-00:00",
+                   :content "Carrot"}
+   #:shopping-item{:ingredient-id "e0fb50e5-ec7f-4b80-9456-e9f9d2fbd68f",
+                   :id #uuid "bceadd9e-9f62-479b-a4cd-377cb564a691",
+                   :status :open,
+                   :created-at #inst "2024-01-20T12:35:00.340-00:00",
+                   :content "Mushroom"}])
 
 (def recipes
   [{:recipe/id "2aa44c10-bf40-476b-b95f-3bbe96a3835f",
@@ -30,4 +51,12 @@
                          {:cooked-with/ingredient "e0fb50e5-ec7f-4b80-9456-e9f9d2fbd68f"
                           :cooked-with/id "60e71736-c2b2-4108-8a17-a5894a213786"
                           :cooked-with/amount-desc "1"
-                          :cooked-with/amount 1.0}]}])
+                          :cooked-with/amount 1.0}
+                         {:cooked-with/ingredient "6c0740a2-24a0-4aa7-9548-60c79bac6fec"
+                          :cooked-with/id #uuid "10d7e058-3fab-49b8-8ab3-26864369e383"
+                          :cooked-with/amount-desc "1"
+                          :cooked-with/amount 1.0},]}])
+
+(def meal {:date #inst "2024-02-06T21:13:01.236-00:00"
+           :type :meal-type/dinner
+           :recipe (first (map (partial recipes/->recipe ingredients) recipes))})
--- test/tech/thomas_sojka/shopping_cards/shopping_items_test.cljs
+++ test/tech/thomas_sojka/shopping_cards/shopping_items_test.cljs
@@ -1,44 +1,17 @@
 (ns tech.thomas-sojka.shopping-cards.shopping-items-test
   (:require [cljs.test :refer-macros [deftest is testing]]
+            [tech.thomas-sojka.shopping-cards.fixtures :as fixtures]
             [tech.thomas-sojka.shopping-cards.shopping-items :as sut]))
 
-(def items
-  [{:shopping-item/ingredient-id "3dc8331b-e1bf-4b59-9883-96bb855f9dfd",
-    :shopping-item/id "2d618d3f-ae1c-4b81-8d2d-adfe67b0eac0",
-    :shopping-item/status :open,
-    :shopping-item/created-at #inst "2024-01-20T12:35:00.340-00:00",
-    :shopping-item/content "1 Dose Kichererbsen"}
-   {:shopping-item/content "Kokosraspeln",
-    :shopping-item/id "bffa7419-515d-4db1-b414-ed25bb156f95",
-    :shopping-item/created-at #inst "2024-01-18T18:16:37.185-00:00",
-    :shopping-item/status :open,
-    :shopping-item/ingredient-id "c181aef6-0e09-43c1-85d1-2aaabdb1ce6a"}
-   {:shopping-item/created-at #inst "2024-01-20T12:35:00.341-00:00",
-    :shopping-item/content "5 Aufback-Brezeln",
-    :shopping-item/status :open,
-    :shopping-item/id "d727f974-8bad-4b00-8e7a-c568181105da",
-    :shopping-item/ingredient-id "d33ad997-4d02-4437-8d35-db8e22fdb4b0"}])
-
-(def sort-order ["d33ad997-4d02-4437-8d35-db8e22fdb4b0"
-                 "c181aef6-0e09-43c1-85d1-2aaabdb1ce6a"
-                 "3dc8331b-e1bf-4b59-9883-96bb855f9dfd"])
+(def sort-order ["6c0740a2-24a0-4aa7-9548-60c79bac6fec"
+                  "bbaba432-0330-4043-90c6-3d3df2fac57b"
+                  "dc1b7bdc-9f9e-4751-b935-468919d39030"
+                  "e0fb50e5-ec7f-4b80-9456-e9f9d2fbd68f"])
 
 (deftest shopping-items
-  (testing "sorts the shopping items"
-    (is
-     (= (sut/sort-shopping-items items sort-order)
-        [{:shopping-item/created-at #inst "2024-01-20T12:35:00.341-00:00",
-          :shopping-item/content "5 Aufback-Brezeln",
-          :shopping-item/status :open,
-          :shopping-item/id "d727f974-8bad-4b00-8e7a-c568181105da",
-          :shopping-item/ingredient-id "d33ad997-4d02-4437-8d35-db8e22fdb4b0"}
-         {:shopping-item/content "Kokosraspeln",
-          :shopping-item/id "bffa7419-515d-4db1-b414-ed25bb156f95",
-          :shopping-item/created-at #inst "2024-01-18T18:16:37.185-00:00",
-          :shopping-item/status :open,
-          :shopping-item/ingredient-id "c181aef6-0e09-43c1-85d1-2aaabdb1ce6a"}
-         {:shopping-item/ingredient-id "3dc8331b-e1bf-4b59-9883-96bb855f9dfd",
-          :shopping-item/id "2d618d3f-ae1c-4b81-8d2d-adfe67b0eac0",
-          :shopping-item/status :open,
-          :shopping-item/created-at #inst "2024-01-20T12:35:00.340-00:00",
-          :shopping-item/content "1 Dose Kichererbsen"}]))))
+  (testing "create shopping items list"
+    (is (= (sut/create-possible-shopping-items sort-order [fixtures/meal])
+           [["6c0740a2-24a0-4aa7-9548-60c79bac6fec" "1 Paprika"]
+            ["bbaba432-0330-4043-90c6-3d3df2fac57b" "1 Onion"]
+            ["dc1b7bdc-9f9e-4751-b935-468919d39030" "1 Carrot"]
+            ["e0fb50e5-ec7f-4b80-9456-e9f9d2fbd68f" "1 Mushroom"]]))))
