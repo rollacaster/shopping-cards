5a3d939d957c0c981c904ed6b2b1f12509064bf0
Thomas Sojka <thsojka@web.de>
2024-02-09 07:14:38 +0100

    Add order for kitchen

--- test/tech/thomas_sojka/shopping_cards/views/sort_ingredients_test.cljs
+++
@@ -1,3 +0,0 @@
-(ns tech.thomas-sojka.shopping-cards.views.sort-ingredients-test
-  (:require [tech.thomas-sojka.shopping-cards.views.sort-ingredients :as sut]
-            [cljs.test :as t :include-macros true]))
