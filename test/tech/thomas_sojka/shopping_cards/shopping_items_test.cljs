649b35c259dcbba9716a63da7cadc5564102966a
Thomas Sojka <thsojka@web.de>
2024-07-01 21:36:05 +0200

    Fix sort order for ingredients

--- test/tech/thomas_sojka/shopping_cards/shopping_items_test.cljs
+++ test/tech/thomas_sojka/shopping_cards/shopping_items_test.cljs
@@ -22,7 +22,6 @@
                              :shopping-item/content
                              :shopping-item/status])
             (sut/create-shopping-list
-             sort-order
              #{"6c0740a2-24a0-4aa7-9548-60c79bac6fec"
                "dc1b7bdc-9f9e-4751-b935-468919d39030"
                "bbaba432-0330-4043-90c6-3d3df2fac57b"
@@ -34,12 +33,12 @@
            [{:shopping-item/ingredient-id "6c0740a2-24a0-4aa7-9548-60c79bac6fec",
              :shopping-item/content "1 Paprika",
              :shopping-item/status :open}
-            {:shopping-item/ingredient-id "bbaba432-0330-4043-90c6-3d3df2fac57b",
-             :shopping-item/content "1 Onion",
-             :shopping-item/status :open}
             {:shopping-item/ingredient-id "dc1b7bdc-9f9e-4751-b935-468919d39030",
              :shopping-item/content "1 Carrot",
              :shopping-item/status :open}
             {:shopping-item/ingredient-id "e0fb50e5-ec7f-4b80-9456-e9f9d2fbd68f",
              :shopping-item/content "1 Mushroom",
+             :shopping-item/status :open}
+            {:shopping-item/ingredient-id "bbaba432-0330-4043-90c6-3d3df2fac57b",
+             :shopping-item/content "1 Onion",
              :shopping-item/status :open}]))))
